ff6eee0276fb4187acb9b1cbb00da03c109a9892
GuruMahajan <156059551+GuruMahajan@users.noreply.github.com>
2024-06-06 08:57:03 +0000

    container config done

---
+++ 01_basic/01_vaiables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "guru@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+console.log(accountId);
+
+accountEmail = "mahajan@gmail.com"
+accountPassword = "4878"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
