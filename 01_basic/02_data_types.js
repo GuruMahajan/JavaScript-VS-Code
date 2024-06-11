a73a11c631483819b5041502e09d956c107e4648
GuruMahajan <156059551+GuruMahajan@users.noreply.github.com>
2024-06-11 04:01:02 +0000

    datatypes

---
+++ 01_basic/02_data_types.js
@@ -0,0 +1,19 @@
+let name = "Guru"
+let age = 21
+let isloggedIn = true
+
+//Premitive Data Type
+//number has range i.e. 2 is to power of 53
+//bigint is used in bigger number eg. stokc market
+//strings are also used in "" and ''
+//boolean => true or flase
+//null => this is a standalone value
+//undefined => the value isn't assigned yet
+//symbol => this is mostly used is react to create uniqueness
+
+//
+//object 
+
+//to check data type use :
+console.log(typeof "guru");
+console.log(typeof 18);
