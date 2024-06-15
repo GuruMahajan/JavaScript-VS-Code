4fab37d87c4e6e1a4c59728854d5df281f754fd0
GuruMahajan <156059551+GuruMahajan@users.noreply.github.com>
2024-06-15 03:54:14 +0000

    conversion of data types

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,5 +23,29 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************Operations**************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Guru"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+let gameCounter = 100
+// ++gameCounter;  => Prefix
+gameCounter++; //  =>  Postfix
+console.log(gameCounter);
