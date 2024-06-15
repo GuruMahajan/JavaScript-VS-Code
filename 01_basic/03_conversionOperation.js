e6eb3f84a4c7b2d3f98fc067be071e0c1c95a863
GuruMahajan <156059551+GuruMahajan@users.noreply.github.com>
2024-06-15 03:21:44 +0000

    Conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+ let score = true
+ //console.log(score);
+ //console.log(typeof score);
+
+let valueInNumber = Number(score);
+ //console.log(valueInNumber);
+ //console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1 ; false => 0
+
+let isLoggedIn = "Guru"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ //console.log(booleanIsLoggedIn);
+ //console.log(typeof booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Guru" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
