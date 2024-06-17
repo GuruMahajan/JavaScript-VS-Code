97b54d33aa314320edb78178ccfc9442e99146ec
GuruMahajan <156059551+GuruMahajan@users.noreply.github.com>
2024-06-17 04:15:48 +0000

    datatypes summary done

---
+++ 01_basic/datatypes_summay.js
@@ -0,0 +1,20 @@
+// Primitive Data Types
+// String, Number, Boolean, null, undefind, Symbol, BigInt
+
+let username = "Guru"
+const score = 100
+// const score = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let useremail;
+// let useremail = undefind
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 123456789101112n
+
+
+// Reference Data Type (Non-Primitive)
+// Arrray, Objects, Functions
